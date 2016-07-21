2fffa335b67807f3fb81fcbd3e30080c9a086fdc
Chad Walker <chad@chad-cat-lore-eddie.com>
2016-07-21 00:04:21 -0500

    rearrange Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 import PackageDescription
 
 let package = Package(
-	name: "Redbird",
-	targets: [
-		Target(name: "Redbird"),
-		Target(name: "RedbirdExample", dependencies: ["Redbird"])
-	],
-	dependencies: [
-		.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 10)
-	]
+  name: "Redbird",
+  targets: [
+    Target(name: "Redbird"),
+    Target(name: "RedbirdExample", dependencies: ["Redbird"])
+  ],
+  dependencies: [
+    .Package(url: "https://github.com/chad3814/Socks.git", majorVersion: 0, minor: 8)
+  ]
 )
